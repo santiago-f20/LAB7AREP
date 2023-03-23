ebe834f33421e39205fe4c2828b83b19dc9dda86
Santiago Fetecua <santiago.fetecua@mail.escuelaing.edu.co>
2023-03-23 14:59:31 -0500

    update

--- src/main/java/edu/eci/arep/HelloWorld.java
+++ src/main/java/edu/eci/arep/HelloWorld.java
@@ -1,12 +1,18 @@
 package edu.eci.arep;
 
 import static spark.Spark.*;
+import static edu.eci.arep.SecureURLReader.*;
 
 public class HelloWorld {
+    // java -jar app-1.0-SNAPSHOT-jar-with-dependencies.jar
+    // java -cp ".target/classes:.target/target/dependency/*"
+    // edu.eci.arep.HelloWorld
     public static void main(String[] args) {
-        secure(getKeyStore(), getKeyStorePwd(), null, null);
         port(getPort());
-        get("/hello", (req, res) -> "Hello Heroku");
+        secure(getKeyStore(), getKeyStorePwd(), null, null);
+        loadTrustStore(getTrustStore(), getKeyStorePwd());
+        get("hello", (req, res) -> "Hello " + getName() + "!");
+        get("remote", (req, res) -> readURL(getLink()));
     }
 
     static int getPort() {
@@ -28,5 +34,26 @@ public class HelloWorld {
             return System.getenv("KEYSTOREPWD");
         }
         return "santi1234"; // returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+    private static String getTrustStore() {
+        if (System.getenv("TSTORE") != null) {
+            return System.getenv("TSTORE");
+        }
+        return "keys/myTrustStore.p12";
+    }
+
+    private static String getLink() {
+        if (System.getenv("LINK") != null) {
+            return System.getenv("LINK");
+        }
+        return "";
+    }
+
+    private static String getName() {
+        if (System.getenv("NAME") != null) {
+            return System.getenv("NAME");
+        }
+        return "World";
     }
 }
